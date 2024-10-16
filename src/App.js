b2f7d6539aa3bf05f338dbb5622d0fd6f7759c98
Nevil556 <manastaalajbekov8@gmail.com>
2024-10-16 17:29:20 +0600

    Init start

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+
+function App() {
+
+  const [count, setCount] = useState(0)
+    function handleIncrement() {
+      setCount(prev => prev + 1)
+    }
+    function handleDecrement() {
+        setCount(prev => prev - 1)
+    }
+
+  return (
+    <div className='wrapper'>
+      <h1 className='heading'>
+          {count}
+      </h1>
+        <div className="buttons">
+
+            <button className='button' onClick={handleDecrement}>
+                -
+            </button>
+            <button className='button' onClick={handleIncrement}>
+                +
+            </button>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
